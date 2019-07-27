5043568b21d838407f3dd945cbb8a1b10c6b48ba
BuildTools <unconfigured@null.spigotmc.org>
2019-07-26 20:37:43 -0400

    Fixed Chat

--- PayPass/src/ca/fastis/PayPass.java
+++ PayPass/src/ca/fastis/PayPass.java
@@ -5,17 +5,14 @@ import org.bukkit.Server;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import net.milkbowl.vault.chat.Chat;
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.permission.Permission;
 
 public class PayPass extends JavaPlugin{
 	static Server server;
 	static ConsoleCommandSender console;
-	static Economy econ = null;
-	private static Permission perms = null;
-	private static Chat chat = null;
+	static Economy econ;
+	private static Permission perms;
 	
 	@Override
 	public void onEnable() {
@@ -27,7 +24,6 @@ public class PayPass extends JavaPlugin{
 			return;
 		}
 		setupPermissions();
-		setupChat();
 		server.getPluginManager().registerEvents(new EventListener(this), this);
 		console.sendMessage(ChatColor.GREEN + "[" + getDescription().getName() + "] Enabled!");
 	}
@@ -42,12 +38,6 @@ public class PayPass extends JavaPlugin{
 		}
 		econ = rsp.getProvider();
 		return econ != null;
-	}
-	
-	private boolean setupChat() {
-		RegisteredServiceProvider<Chat> rsp = getServer().getServicesManager().getRegistration(Chat.class);
-		chat = rsp.getProvider();
-		return chat != null;
 	}
 	
 	private boolean setupPermissions() {
